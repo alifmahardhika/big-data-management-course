af03766ba3b8e83ff5d76d9427b01b37a5b18fd1
alifmahardhika <alif.mahardhika@ui.ac.id>
2020-09-26 13:43:39 +0700

    specify number of reducer used

--- WordCount.java
+++ WordCount.java
@@ -45,6 +45,7 @@ public class WordCount {
         job.setReducerClass(MyReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
+        job.setNumReduceTasks(3);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
