44dda755eec9b878073010e07066f118d508d791
alifmahardhika <54898652+alifmahardhika@users.noreply.github.com>
2020-10-22 15:19:06 +0700

    add wordcount_spark

---
+++ jupyter_tutorials/tutorial spark/wordcount_spark.py
@@ -0,0 +1,8 @@
+# NOTE gabuat dijalanin dilocal
+from operator import add
+from pyspark import SparkContext
+sc = SparkContext("local[*]", "example")
+f = sc.textFile("data/README.md")
+wc = f.flatMap(lambda x: x.split(' ')).map(lambda x:
+                                           (x, 1)).reduceByKey(add)
+wc.saveAsTextFile("wc_out")
