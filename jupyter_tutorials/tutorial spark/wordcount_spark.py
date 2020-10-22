0d7573196b586ce4e4b2417c35d7139ca8e628ce
alifmahardhika <54898652+alifmahardhika@users.noreply.github.com>
2020-10-22 15:25:56 +0700

    error in worcount spark

--- jupyter_tutorials/tutorial spark/wordcount_spark.py
+++ jupyter_tutorials/tutorial spark/wordcount_spark.py
@@ -3,6 +3,5 @@ from operator import add
 from pyspark import SparkContext
 sc = SparkContext("local[*]", "example")
 f = sc.textFile("data/README.md")
-wc = f.flatMap(lambda x: x.split(' ')).map(lambda x:
-                                           (x, 1)).reduceByKey(add)
+wc = f.flatMap(lambda x: x.split(' ')).map(lambda x: (x, 1)).reduceByKey(add)
 wc.saveAsTextFile("wc_out")
