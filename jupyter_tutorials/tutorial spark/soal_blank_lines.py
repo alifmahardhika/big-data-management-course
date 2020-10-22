01a3584cc8cdfc61d22918f66558ed25a04bd2e4
alifmahardhika <54898652+alifmahardhika@users.noreply.github.com>
2020-10-22 16:37:33 +0700

    create blank line counter

---
+++ jupyter_tutorials/tutorial spark/soal_blank_lines.py
@@ -0,0 +1,20 @@
+from pyspark import SparkConf,SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql import *
+
+conf = SparkConf().setMaster("local[*]").setAppName("Accumulator")
+spark = SparkSession.builder.getOrCreate()
+
+blk_cnt = spark.sparkContext.accumulator(0)
+
+print(“Initial Value of Accumulaor: ” , blk_cnt.value)
+
+def Blank_lines(line):
+    if(len(line) == 0):
+        blk_cnt.add(1)
+
+file = spark.sparkContext.textFile("data/README.md")
+
+r1 = file.foreach(lambda x: Blank_lines(x))
+
+print(“Updated Value of Accumulaor: ” ,blk_cnt.value)
