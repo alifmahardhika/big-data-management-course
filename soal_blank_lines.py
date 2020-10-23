b4796dcb09c7ece13b33572e784c3ec2df965fc5
alifmahardhika <54898652+alifmahardhika@users.noreply.github.com>
2020-10-23 22:51:12 +0700

    rapihrapih

--- soal_blank_lines.py
+++ soal_blank_lines.py
@@ -5,16 +5,16 @@ from pyspark.sql import *
 conf = SparkConf().setMaster("local[*]").setAppName("Accumulator")
 spark = SparkSession.builder.getOrCreate()
 
-blk_cnt = spark.sparkContext.accumulator(0)
+blank_count = spark.sparkContext.accumulator(0)
 
 print("blank line starting count:" , blk_cnt.value)
 
-def Blank_lines(line):
+def blank_lines(line):
     if(len(line) == 0):
         blk_cnt.add(1)
 
 file = spark.sparkContext.textFile("README.md")
 
-r1 = file.foreach(lambda x: Blank_lines(x))
+r1 = file.foreach(lambda x: blank_lines(x))
 
-print("blank line final count: " ,blk_cnt.value)
+print("blank line final count: " ,blank_count.value)
